98f21c08f47fee974999b37e4e009d76f95c4e8f
Ekaterina <gaurung505@gmail.com>
2024-03-01 14:51:04 +0300

    hw

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
+function solveEquation(a, b, c){
+  const arr = [];
+  const d = b**2 - 4*a*c;
+  if (d>0) {
+    let x1 = ((-b + Math.sqrt(d))/(2*a));
+    let x2 = ((-b - Math.sqrt(d))/(2*a));
+    arr.push(x1); 
+    arr.push(x2);
+  } else if (d === 0) {
+    const x = (-b/(2*a));
+    arr.push(x);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = percent/100/12;
+  let payment = amount - contribution;
+  let monthlyPayment = payment * (p + (p / (((1 + p)**countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  return +totalAmount.toFixed(2);
 }
+console.log(calculateTotalMortgage(15, 0, 10000, 36));
