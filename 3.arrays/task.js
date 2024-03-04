42423e916b1f8d7516810097203e2aeffa6ffdb8
Ekaterina <gaurung505@gmail.com>
2024-03-04 18:20:35 +0300

    hw3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
-function compareArrays(arr1, arr2) {
-  
-}
+function compareArrays(arr1, arr2){
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    return arr1.every((value, index) =>{
+      console.log(value, arr2[index]);
+      return value === arr2[index];
+    });
+  }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  function getUsersNamesInAgeRange(users, gender){
+    let result = users.filter(user => user.gender === gender);
+    let result2 = result.map(user => user.age);
+    let result3 = result2.reduce((acc, item, index, arr) => {
+      acc += item;
+      if (index === arr.length - 1) {
+        return acc / arr.length;
+      } else {
+        return acc;
+      }
+    }, 0);
+    return result3;
+  }
